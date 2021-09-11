a09bfa985425c94b183f977106b94a453440d68a
colombolucas <colombo.lucasit@gmail.com>
2021-09-10 21:02:43 -0300

    Startup projeto

---
+++ src/main/java/br/senac/ecommerceapiprodutos/ECommerceApiProdutosApplication.java
@@ -0,0 +1,13 @@
+package br.senac.ecommerceapiprodutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApiProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApiProdutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/senac/ecommerceapiprodutos/ECommerceApiProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.ecommerceapiprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApiProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
